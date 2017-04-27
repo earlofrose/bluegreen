38fe8b8242243e85e1636452c71edd38c26b0245
earlofrose <josh.rozier@stjude.org>
2017-04-27 14:35:26 -0500

    Update image.php

--- image.php
+++ image.php
@@ -8,9 +8,9 @@ $deployment = getenv("COLOR");
 if ( $deployment == 'blue') {
   #$color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 255, 0);
+  #$color = imagecolorallocate($im, 0, 255, 0);
 } else {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  #$color = imagecolorallocate($im, 0, 0, 255);
 }
 
 // Draw a filled rectangle
